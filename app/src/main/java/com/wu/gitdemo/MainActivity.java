1d4424d7fdc4728433afe8cf3ac12235073af0b4
Wu <2681159485@qq.com>
2016-04-29 14:58:47 +0800

    第3次提交1

--- app/src/main/java/com/wu/gitdemo/MainActivity.java
+++ app/src/main/java/com/wu/gitdemo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv2;
+        TextView tv1= (TextView) findViewById(R.id.tv);
     }
 }
